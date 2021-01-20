5a2c8605662eb9fd1ffa8ff969325383c691a18c
rabbitism <popmessiah@126.com>
2021-01-21 00:30:12 +0800

    feat: top-level programming demo

---
+++ Demostration/TopLevelPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TopLevelPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
